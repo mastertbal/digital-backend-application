4a3e05c1cf1cc28e788a9356770aba17d5db2b4c
Balogun Oluwatobi John <mastertbal@gmail.com>
2026-07-19 15:10:11 +0100

    Revert "Feat/docs test"

--- src/main/java/com/groupa/digitalbackendapplication/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/groupa/digitalbackendapplication/service/impl/CustomerServiceImpl.java
@@ -75,8 +75,8 @@ public class CustomerServiceImpl implements CustomerService {
                 .gender(gender)
                 .dateOfBirth(dateOfBirth)
                 .address(address)
-                .bvn(bvn != null && bvn.isBlank() ? null : bvn)
-                .nin(nin != null && nin.isBlank() ? null : nin)
+                .bvn(bvn)
+                .nin(nin)
                 .build();
         Customer savedCustomer = customerRepository.save(customer);
         return new SavedCustomerResponse(savedCustomer.getId());
