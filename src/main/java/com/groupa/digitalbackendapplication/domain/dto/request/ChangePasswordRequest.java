07bacff21b172a19fd83220817d2c13cb8b9f9ae
Abdulroqib-Adeleke <adelekeabdulroqib29@gmail.com>
2026-08-19 17:28:16 -0700

    merged master to implement password policy in password reset implementations

--- src/main/java/com/groupa/digitalbackendapplication/domain/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/groupa/digitalbackendapplication/domain/dto/request/ChangePasswordRequest.java
@@ -1,10 +1,15 @@
 package com.groupa.digitalbackendapplication.domain.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record ChangePasswordRequest(
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z]).{10,15}$",
+                message = "Password should contain at least one number, one letter and must be 10 to 15 characters long")
         @NotNull(message = "provide a valid password")
         String newPassword,
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z]).{10,15}$",
+                message = "Password should contain at least one number, one letter and must be 10 to 15 characters long")
         @NotNull(message = "Reenter your new password")
         String confirmPassword) {
 }
